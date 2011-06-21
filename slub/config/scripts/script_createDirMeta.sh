fefa7260bc2589d3d28b764b43d6029c8558a83c
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2011-06-21 15:36:47 +0200

    Refactored directory creation script

--- slub/config/scripts/script_createDirMeta.sh
+++ slub/config/scripts/script_createDirMeta.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-# Logging einschalten?
+# Enable logging
 Debug=1
+
+# Include error level handling
 source `dirname $0`/errorlevel.sh
 
-# erste Variable: Verzeichnis
-Verzeichnis="$1"
+# Fetch directory name from command-line argument
+Directory="$1"
 
-#echo $Verzeichnis
-if [ $Debug -eq 1 ]; then logger -p user.info -t $0 "/bin/mkdir -m 0775 $Verzeichnis"; fi
-/bin/mkdir -m 0775 "$Verzeichnis"
+# Assemble options 
+Verbose=
+Filemode="-m 0775"
+
+if [ ${Debug} -eq 1 ]; then
+    Verbose="-v"
+fi
+
+# Call mkdir and capture output
+Out=`/bin/mkdir ${Verbose} ${Filemode} "${Directory}" 2>&1`
+
+# Get last command error level
 Errorlevel=$?
+
+# Log mkdir output if Debug is enabled
+if [ ${Debug} -eq 1 ] && [ -n "${Out}" ] ; then
+    logger -p user.info ${Out}
+fi
+
+# Call errorlevel function (log error to syslog)
 errorlevel
