29ff2cdd3d4a460387bcf28277274a6a3d1853ce
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2011-07-11 15:43:54 +0200

    fix double symlink creation

--- slub/config/scripts/script_createDirMetaSymLink.sh
+++ slub/config/scripts/script_createDirMetaSymLink.sh
@@ -3,6 +3,14 @@
 # Enable logging
 Debug=1
 
+# Assemble options 
+Verbose=
+Filemode="-m 0775"
+
+if [ ${Debug} -eq 1 ]; then
+    Verbose="-v"
+fi
+
 # Include error level handling
 source `dirname $0`/errorlevel.sh
 
@@ -13,68 +21,90 @@ if [ -z "$1" ]; then
 fi
 
 # Fetch directory name from command-line argument and remove trailing slash
-Symlink=${1%/}
-
-# Exit if symlink exists
-if [ -L $Symlink ]; then
-    logger -p user.info -t $0 "Target already exists."
-    exit 1
-fi
-
-# Assemble options 
-Verbose=
-Filemode="-m 0775"
-
-if [ ${Debug} -eq 1 ]; then
-    Verbose="-v"
-fi
+Directory=${1%/}
 
 # Extract first number sequence as Goobi process ID
-ProcessId=`expr match "$Symlink" '[a-zA-Z0-9\/]*\/\([0-9]\+\)'`
+ProcessId=`expr match "$Directory" '[a-zA-Z0-9\/]*\/\([0-9]\+\)'`
 
-# Determine storage area
-Area="partitions/"`expr $ProcessId / 10000`
+# Base directory is Directory path up to Goobi process ID
+BaseDirectory=${Directory%/$ProcessId*}
 
-# Base directory is Symlink path without Goobi process ID and following parts
-BaseDirectory=${Symlink%$ProcessId*}
+# Determine storage partition
+PartitionDirectory=${BaseDirectory}"/partitions/"`expr $ProcessId / 10000`"/"${ProcessId}
 
-# Determine target directory
-TargetDirectory=${BaseDirectory}${Area}/${ProcessId}
+# Determine Symlink
+Symlink=${BaseDirectory}/${ProcessId}
 
-# Build commands
-Mkdir="/bin/mkdir -p ${Verbose} ${Filemode} ${TargetDirectory}"
-Ln="/bin/ln ${Verbose} -s ${TargetDirectory} ${Symlink}"
 
-# Call mkdir, log command and capture output
-if [ $Debug -eq 1 ]; then
-    logger -p user.info -t $0 "${Mkdir}"
-fi
-Out=`${Mkdir} 2>&1`
+# Create partition directory
+if [ ! -d ${PartitionDirectory} ]; then
 
-# Log mkdir output if Debug is enabled
-if [ ${Debug} -eq 1 ] && [ -n "${Out}" ] ; then
-    logger -p user.info -t $0 "${Out}"
-fi
+    Mkdir="/bin/mkdir -p ${Verbose} ${Filemode} ${PartitionDirectory}"
 
-# Get last command error level
-Errorlevel=$?
+    # Call mkdir, log command and capture output
+    if [ $Debug -eq 1 ]; then
+        logger -p user.info -t $0 "${Mkdir}"
+    fi
+    Out=`${Mkdir} 2>&1`
 
-# Call errorlevel function (signal error to syslog)
-errorlevel
+    # Log mkdir output if Debug is enabled
+    if [ ${Debug} -eq 1 ] && [ -n "${Out}" ] ; then
+        logger -p user.info -t $0 "${Out}"
+    fi
 
-# Call ln, log command and capture output
-if [ $Debug -eq 1 ]; then
-    logger -p user.info -t $0 "${Ln}"
-fi
-Out=`${Ln} 2>&1`
+    # Get last command error level
+    Errorlevel=$?
 
-# Log ln output if Debug is enabled
-if [ ${Debug} -eq 1 ] && [ -n "${Out}" ] ; then
-    logger -p user.info -t $0 "${Out}"
+    # Call errorlevel function (signal error to syslog)
+    errorlevel
+
 fi
 
-# Get last command error level
-Errorlevel=$?
+# Create symlink to partition directory
+if [ ! -L ${Symlink} ]; then
 
-# Call errorlevel function (signal error to syslog)
-errorlevel
+    Ln="/bin/ln ${Verbose} -s ${PartitionDirectory} ${Symlink}"
+
+    # Call ln, log command and capture output
+    if [ $Debug -eq 1 ]; then
+        logger -p user.info -t $0 "${Ln}"
+    fi
+    Out=`${Ln} 2>&1`
+
+    # Log ln output if Debug is enabled
+    if [ ${Debug} -eq 1 ] && [ -n "${Out}" ] ; then
+        logger -p user.info -t $0 "${Out}"
+    fi
+
+    # Get last command error level
+    Errorlevel=$?
+
+    # Call errorlevel function (signal error to syslog)
+    errorlevel
+
+fi
+
+
+# Create final directory through symlink
+if [ ! -d ${Directory} ]; then
+
+    Mkdir="/bin/mkdir -p ${Verbose} ${Filemode} ${Directory}"
+
+    # Call mkdir, log command and capture output
+    if [ $Debug -eq 1 ]; then
+        logger -p user.info -t $0 "${Mkdir}"
+    fi
+    Out=`${Mkdir} 2>&1`
+
+    # Log mkdir output if Debug is enabled
+    if [ ${Debug} -eq 1 ] && [ -n "${Out}" ] ; then
+        logger -p user.info -t $0 "${Out}"
+    fi
+
+    # Get last command error level
+    Errorlevel=$?
+
+    # Call errorlevel function (signal error to syslog)
+    errorlevel
+
+fi
