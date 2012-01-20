c831902121555af5ad90b0de6fcaf3591193ea90
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2012-01-20 15:19:31 +0100

    add create statement for missing table "benutzereigenschaften"

--- db/db_migrate_153_170.sql
+++ db/db_migrate_153_170.sql
@@ -1,3 +1,8 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+
+-- NOTE: remove comment hyphens only if these tables are empty
+
 --drop table metadatenkonfigurationen;
 --drop table projekte;
 
@@ -22,3 +27,17 @@ alter table werkstueckeeigenschaften modify Wert varchar(255);
 alter table werkstueckeeigenschaften modify IstObligatorisch bit;
 alter table werkstueckeeigenschaften modify Auswahl varchar(255);
 
+CREATE TABLE `benutzereigenschaften` (
+  `benutzereigenschaftenID` int(11) NOT NULL AUTO_INCREMENT,
+  `Titel` varchar(255) DEFAULT NULL,
+  `Wert` varchar(255) DEFAULT NULL,
+  `IstObligatorisch` bit(1) DEFAULT NULL,
+  `DatentypenID` int(11) DEFAULT NULL,
+  `Auswahl` varchar(255) DEFAULT NULL,
+  `creationDate` datetime DEFAULT NULL,
+  `BenutzerID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`benutzereigenschaftenID`),
+  KEY `FK963DAE0F8896477B` (`BenutzerID`),
+  KEY `FK963DAE0FC44F7B5B` (`BenutzerID`)
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
